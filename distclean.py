c7f382790049937898ec30b8f88ae91d4023a011
John Kirkham <kirkhamj@janelia.hhmi.org>
2018-01-06 01:45:52 -0500

    Add a distclean script
    Provides a way of purging the `out` directory easily. Certainly this can
    be done without a script, but this a nice convenience script.

---
+++ distclean.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import argparse
+import os
+import shutil
+import sys
+
+
+def main(*argv):
+    parser = argparse.ArgumentParser(
+        description="Cleanup any installers in `out`."
+    )
+    args = parser.parse_args(args=argv[1:])
+
+    base_dir = os.path.dirname(os.path.abspath(__name__))
+    out_dir = os.path.join(base_dir, "out")
+
+    if os.path.exists(out_dir):
+        shutil.rmtree(out_dir)
+
+
+if __name__ == "__main__":
+    sys.exit(main(*sys.argv))
