77bf3d386a072d954a51bd8eeee89183fc1e55fc
John Kirkham <kirkhamj@janelia.hhmi.org>
2018-01-08 01:05:59 -0500

    Consolidate CircleCI steps into one script
    Instead of having multiple CircleCI `run`/`command` lines, just have one
    for each step that calls our script. Then run the rest of the steps from
    the script. This should be faster as only one container is initiated.
    Also should be easier to maintain as all the code is in one file.

--- .circleci/run.sh
+++ .circleci/run.sh
@@ -5,3 +5,5 @@ set -x
 conda install -yq constructor jinja2
 
 /home/conda/repo/build.py --python "${PYVER}" --hash md5 sha1 sha256
+/home/conda/repo/check.py out/miniforge-py${PYVER}-*.sh
+/home/conda/repo/distclean.py
