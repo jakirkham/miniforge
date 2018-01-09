a9a73cb685bdd4aadb0f90923f6f26dfe370d491
John Kirkham <kirkhamj@janelia.hhmi.org>
2018-01-09 02:15:58 -0500

    Only generate `sha256` checksums for installers
    There is a bit too much clutter in releases by including this many
    checksums. As `md5` is not cryptographically secure and `sha1` is
    showing weakness, just use `sha256` only. This is what PyPI uses for
    packages and has become increasingly common in conda-forge. Only makes
    sense to follow suit here.

--- .appveyor_scripts/run.bat
+++ .appveyor_scripts/run.bat
@@ -3,5 +3,5 @@
 conda.exe install -yq constructor jinja2
 if errorlevel 1 exit 1
 
-build.py --python "%PYVER%" --hash md5 sha1 sha256
+build.py --python "%PYVER%" --hash sha256
 if errorlevel 1 exit 1
